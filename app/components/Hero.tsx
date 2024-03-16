2817faabd78a81290218753e44ccd8f9bdaa84a8
kodaneflash <jamesonmarcher@gmail.com>
2024-03-16 18:40:39 -0400

    modified:   app/components/Hero.tsx 	modified:   app/globals.css

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -17,7 +17,7 @@ export default async function Hero() {
       <div className="mb-8 flex flex-wrap justify-between md:mb-16">
         <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
-            Top Vendor for <span style={{ background: "linear-gradient(to right, lightblue, navy)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }}>Premium Fake ID's</span>
+            Top Vendor for <span style={{ background: "linear-gradient(to right, black 30%, #0074D9 70%)", WebkitBackgroundClip: "text", WebkitTextFillColor: "transparent" }}>Premium Fake ID's</span>
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
           Our flawless designs, DMV-grade printing materials, & advanced

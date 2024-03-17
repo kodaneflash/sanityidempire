646e969680b599379107c62f439c58a50f02cac4
kodaneflash <jamesonmarcher@gmail.com>
2024-03-16 21:20:59 -0400

    modified:   app/layout.tsx modified:   package-lock.json modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'; // Import GoogleAnalytics
 import "./globals.css";
 import CartProvider from "./components/Providers";
 import Navbar from "./components/Navbar";
 import ShoppingCartModal from "./components/ShoppingCartModal";
-import Footer from "./components/Footer"; // Removed this line to prevent duplication
-import Head from 'next/head'; // Fix this line
+import Footer from "./components/Footer";
+import Head from 'next/head';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -37,19 +38,10 @@ export default function RootLayout({
       <Head>
         <title>{String(metadata.title)}</title>
         <meta name="description" content={String(metadata.description)} />
-        <link rel="icon" href="/favicon.ico" /> {/* Add your favicon link here */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5JHQ9SJ9');`,
-          }}
-        />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Other head elements */}
       </Head>
       <body className={inter.className}>
-        {/* Directly include the GTM NoScript iframe here */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5JHQ9SJ9"
@@ -62,8 +54,9 @@ export default function RootLayout({
           <Navbar />
           <ShoppingCartModal />
           {children}
-        <Footer />
+          <Footer />
         </CartProvider>
+        <GoogleAnalytics gaId="GTM-5JHQ9SJ9" /> {/* Include GoogleAnalytics with your measurement ID */}
       </body>
     </html>
   );
